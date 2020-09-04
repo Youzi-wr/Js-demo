fcb590d54945f04d7bef5bccffcb495eb6447d6c
wangru <1023820943@qq.com>
2020-09-04 15:17:13 +0800

    clear

--- printjs/index.js
+++
@@ -1,7 +0,0 @@
-import printJS from 'print-js';
-
-function click(){
-    printJS('https://192.168.11.99/uz-download/api/file/daemon/temp/attachment?access_token=83c28147-5030-4de2-ad04-7f6fed836224&path=printer_1173755242219520/3e169cc3-55be-41c2-bb27-65e63480d86a')
-}
-
-export default click;
--- projects/bmp.js
+++
@@ -1,9 +0,0 @@
-var bmp = require("bmp-js");
-var fs = require("fs");
-var bmpData = {
-    data, //Buffer
-    width, //Number
-    height //Number
-};
-var rawData = bmp.encode(bmpData); //defaults to no compression
-fs.WriteFileSync('./image.bmp', rawData.data);
